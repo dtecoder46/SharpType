16ed2d90a1074bf996c33adbba5bc5f54b160388
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-09-04 10:11:22 -0400

    Added new input example

--- input.cs
+++ input.cs
@@ -2,7 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    string ! hello = "bruh";
+    const ! string ! hello = "bruh";
     Console.Write(hello);
     //&bruh
   }
